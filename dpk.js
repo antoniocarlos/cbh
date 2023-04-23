0bbc7f5a77f4b827a1c3dd2bbdcadaf88ea4353d
Antonio Carlos Santos Thiele <antoniocarloss60@gmail.com>
2023-04-23 10:56:15 -0300

    dpk refactor

--- dpk.js
+++ dpk.js
@@ -1,28 +1,40 @@
-const crypto = require("crypto");
+import crypto from 'crypto';
 
-exports.deterministicPartitionKey = (event) => {
-  const TRIVIAL_PARTITION_KEY = "0";
-  const MAX_PARTITION_KEY_LENGTH = 256;
-  let candidate;
+const TRIVIAL_PARTITION_KEY = '0';
+const MAX_PARTITION_KEY_LENGTH = 256;
 
-  if (event) {
-    if (event.partitionKey) {
-      candidate = event.partitionKey;
-    } else {
-      const data = JSON.stringify(event);
-      candidate = crypto.createHash("sha3-512").update(data).digest("hex");
-    }
+export const hashData = (data) => {
+  return crypto.createHash('sha3-512').update(data).digest('hex');
+};
+
+const getPartitionKeyCandidate = (event) => {
+  if (!event) {
+    return TRIVIAL_PARTITION_KEY;
+  }
+  
+  if (event.partitionKey) {
+    return event.partitionKey;
   }
 
-  if (candidate) {
-    if (typeof candidate !== "string") {
-      candidate = JSON.stringify(candidate);
-    }
-  } else {
-    candidate = TRIVIAL_PARTITION_KEY;
-  }
+  return hashData(JSON.stringify(event));
+};
+
+const ensureString = (candidate) => {
+  return (typeof candidate === 'string') ? candidate : JSON.stringify(candidate);
+};
+
+const truncateOrHash = (candidate) => {
   if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
-    candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
+    return hashData(candidate);
   }
+
   return candidate;
+};
+
+export const deterministicPartitionKey = (event) => {
+  const candidate = getPartitionKeyCandidate(event);
+  const stringCandidate = ensureString(candidate);
+  const finalCandidate = truncateOrHash(stringCandidate);
+  
+  return finalCandidate;
 };
